c2a2ef39ced78fddbdb52bfc569fa38b7621ff20
Jinnie <hjk7639@gmail.com>
2024-09-23 21:16:25 +0900

    Routes & Navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
+import Navigation from '@/components/navigation';
+import '../app/globals.css';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </html>
+  );
 }
